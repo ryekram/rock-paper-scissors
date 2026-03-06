8a8a7a2a8a78890edd28bd3b32995d6b07e5fe62
ryekram <reymark.magsanay@gmail.com>
2026-03-06 14:20:51 +0800

    feat: implement rock paper scissor logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,70 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM fully loaded and parsed");
+
+  let rounds = 5;
+  let humanScore = 0;
+  let computerScore = 0;
+  let handleSelection = ["rock", "paper", "scissor"];
+  let playBtn = document.querySelector(".playBtn");
+
+  let getHumanChoice = () => {
+    let choice = window.prompt("Please select your hand: ");
+    let result = validateAndLowerCase(choice);
+    console.log(result);
+    if (!result) {
+      return getHumanChoice();
+    }
+    return result;
+  };
+
+  let getComputerChoice = () => {
+    let rand = Math.floor(Math.random() * 3);
+    console.log(`randomize : ${ rand }`)
+    let choice = handleSelection[rand];
+    return choice;
+  };
+
+  let validateAndLowerCase = (text) => {
+    if (text === null) return false;
+    text = text.toLowerCase();
+    if (!handleSelection.includes(text)) return false;
+    return text;
+  };
+
+  let playRound = (humanChoice, computerChoice) => {
+    let humanWon = false;
+    if (humanChoice === computerChoice) {
+      console.log(
+        `It's a tie! player: ${humanChoice},  computer: ${computerChoice}`,
+      );
+      return;
+    }
+    if ((humanChoice === "rock") & (computerChoice === "scissor")) {
+      humanScore += 1;
+      humanWon = true;
+    } else if ((humanChoice === "paper") & (computerChoice === "rock")) {
+      humanScore += 1;
+      humanWon = true;
+    } else if ((humanChoice === "scissor") & (computerChoice === "paper")) {
+      humanScore += 1;
+      humanWon = true;
+    } else {
+      computerScore += 1;
+    }
+    console.log(
+      `You ${humanWon ? "Win" : "Lose"}! ${humanChoice} beats ${computerChoice}`,
+    );
+  };
+  let handleClick = () => {
+    console.log("click");
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    console.log(`PlayerScore: ${humanScore}`)
+    console.log(`ComputerScore: ${computerScore}`)
+  };
+  playBtn.addEventListener("click", handleClick);
+
+  //   for (let index = 0; index < rounds; index++) {
+  //   }
 });
