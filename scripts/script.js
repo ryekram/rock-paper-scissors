3c00fbd2a2f6e2ef0bd1deaee931f7a9d105e7e1
ryekram <reymark.magsanay@gmail.com>
2026-03-06 19:32:37 +0800

    refactor: changed notification popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,32 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  console.log("DOM fully loaded and parsed");
+  gsap.registerPlugin(ScrollTrigger, CustomEase, CustomBounce);
 
   let rounds = 5;
   let humanScore = 0;
   let computerScore = 0;
   let handleSelection = ["rock", "paper", "scissor"];
   let playBtn = document.querySelector(".play__button");
+  let rockBtn = document.querySelector(".hand__selection-rock");
+  let paperBtn = document.querySelector(".hand__selection-paper");
+  let scissorBtn = document.querySelector(".hand__selection-scissor");
+  let handSelectionArea = document.querySelector(".hand__selection");
+  let humanSpanScore = document.querySelector(".stats__player-score");
+  let computerSpanScore = document.querySelector(".stats__computer-score");
+  let notification = document.querySelector(".notification");
+  let notificationText = document.querySelector(".notification__text");
 
-  let getHumanChoice = () => {
-    let result = null;
-    while (!result) {
-      let choice = window.prompt("Please select your hand: ");
-      result = validateAndLowerCase(choice);
+  let getHumanChoice = (e) => {
+    selected = e.target;
+    let choice;
+
+    if (e.target.classList.contains("hand__selection-rock")) {
+      choice = "rock";
+    } else if (e.target.classList.contains("hand__selection-paper")) {
+      choice = "paper";
+    } else {
+      choice = "scissor";
     }
-    return result;
+    return choice;
   };
 
   let getComputerChoice = () => {
@@ -30,12 +43,36 @@ document.addEventListener("DOMContentLoaded", (event) => {
     return text;
   };
 
+  let notificationAnimation = (addClass = "") => {
+    console.log(addClass)
+    let tl = gsap.timeline();
+    tl.to(notification, {
+      className: "notification " + addClass,
+      duration: 1
+    })
+    .to(notification, {
+      y: 50,
+      opacity: 1,
+      ease: "back.out",
+      duration: 0.5,
+    }, "<").to(notification, {
+      y: 0,
+      opacity: 0,
+      ease: "back.in",
+      duration: 0.5,
+      delay: 0.8,
+      // className: "notification ",
+    });
+  };
+
   let playRound = (humanChoice, computerChoice) => {
     let humanWon = false;
     if (humanChoice === computerChoice) {
-      console.log(
-        `It's a tie! player: ${humanChoice},  computer: ${computerChoice}`,
-      );
+      notification.innerHTML = `It's a tie! player: ${humanChoice},  computer: ${computerChoice}`;
+      notificationAnimation()
+      // console.log(
+      //   `It's a tie! player: ${humanChoice},  computer: ${computerChoice}`,
+      // );
       return;
     }
     if ((humanChoice === "rock") & (computerChoice === "scissor")) {
@@ -50,19 +87,36 @@ document.addEventListener("DOMContentLoaded", (event) => {
     } else {
       computerScore += 1;
     }
-    console.log(
-      `You ${humanWon ? "Win" : "Lose"}! ${humanChoice} beats ${computerChoice}`,
-    );
+
+    notification.innerHTML = `You ${humanWon ? "Win" : "Lose"}! ${humanChoice} beats ${computerChoice}`;
+    let winLoseClass = humanWon ? "win" : "lose"
+    console.log(winLoseClass)
+    notificationAnimation(winLoseClass)
+    // console.log(
+
+    //   `You ${humanWon ? "Win" : "Lose"}! ${humanChoice} beats ${computerChoice}`,
+    // );
   };
   let handleClick = () => {
-    console.log("click");
-    let humanSelection = getHumanChoice();
+    handSelectionArea.style.display = "block";
+
+    // let humanSelection = getHumanChoice();
+    // let computerSelection = getComputerChoice();
+    // playRound(humanSelection, computerSelection);
+    // console.log(`PlayerScore: ${humanScore}`);
+    // console.log(`ComputerScore: ${computerScore}`);
+  };
+
+  let handleHandChoice = (e) => {
+    let humanSelection = getHumanChoice(e);
     let computerSelection = getComputerChoice();
     playRound(humanSelection, computerSelection);
-    console.log(`PlayerScore: ${humanScore}`);
-    console.log(`ComputerScore: ${computerScore}`);
+    humanSpanScore.innerHTML = humanScore;
+    computerSpanScore.innerHTML = computerScore;
   };
+
   playBtn.addEventListener("click", handleClick);
+  handSelectionArea.addEventListener("click", handleHandChoice);
 
   //   for (let index = 0; index < rounds; index++) {
   //   }
